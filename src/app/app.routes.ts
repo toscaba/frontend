c7b74121580e72b6c5452f2b9c2de54ed977c281
kyklospi <rachmawati.atika@gmail.com>
2025-01-11 20:06:15 +0100

    add reservation component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { EateryComponent } from "./eatery/eatery.component";
 import { LoginComponent } from './login/login.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 export const routes: Routes = [
         { path: '', component: EateryComponent }, 
-        { path: 'login', component: LoginComponent }
+        { path: 'login', component: LoginComponent },
+        { path: 'eateries/:eateryId/reservation', component: ReservationComponent }
 ];
