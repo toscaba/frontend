5da7861dad5c3d51b46665cd40a2154df3720cc5
toscabaumgartner <toba2589@bht-berlin.de>
2025-01-15 20:25:06 +0100

    Updated login component with new API integration

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,56 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiService, Customer } from '../api.service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import { Customer } from '../customer/customer';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  router: Router;
-  @Input() firstName: string | undefined;
-  @Input() lastName: string | undefined;
-  @Input() phoneNumber: string | undefined;
-  @Input() email: string | undefined;
-  @Input() payment: string | undefined;
+  firstName: string = '';
+  lastName: string = '';
+  phoneNumber: string = '';
+  email: string = '';
+  payment: string = '';
 
-  constructor(private _router: Router) {
-    this.router = _router;
-    // this.loginForm = this.formBuilder.group({
-    //   firstName: ['', Validators.required],
-    //   lastName: ['', Validators.required],
-    //   phoneNumber: ['', Validators.required]
-    // });
-  }
+  constructor(private apiService: ApiService, private router: Router) {}
 
-  onSubmit() {
-    // if (this.loginForm.valid) {
-      // const { firstName, lastName, phoneNumber, email } = this.loginForm.value;
-      // TODO: connect to API create user
-      // this.apiService.createCustomer(firstName, lastName, phoneNumber).subscribe(
-      //   (customer) => {
-      //     // Handle successful login
-      //     console.log('Login successful:', customer);
-      //   },
-      //   (error) => {
-      //     // Handle login failure
-      //     console.error('Login failed:', error);
-      //   }
-      // );
-      console.log("FIRST NAME", this.firstName);
-      console.log("LAST NAME", this.lastName);
-      console.log("PHONE", this.phoneNumber);
-      console.log("EMAIL", this.email);
-      console.log("PAYMENT", this.payment);
-      // const customer: Customer = {id: 1, firstName, lastName: this.lastName?, phoneNumber, email, payment: 'CASH'};
-      // this.router.navigateByUrl('/profile/'+ customer.id);
-      this.router.navigateByUrl('/profile/'+ 1);
-    // }
+  onSubmit(): void {
+    const newCustomer: Customer = {
+      id: 0, // Backend generiert die ID
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      phoneNumber: this.phoneNumber
+    };
+
+    this.apiService.createCustomer(newCustomer).subscribe({
+      next: (customer) => {
+        console.log('Customer created:', customer);
+        alert('Customer created successfully!');
+        this.router.navigateByUrl(`/profile/${customer.id}`);
+      },
+      error: (err) => {
+        console.error('Error creating customer:', err);
+        alert('Error creating customer. Please try again.');
+      }
+    });
   }
 }
